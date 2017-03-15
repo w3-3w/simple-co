0e4498e5ed484a8898e4364116793c02441ac62c
w3_3w <w3-3w@live.cn>
2017-03-16 03:29:00 +0900

    fix indent
    minor change

--- index.js
+++ index.js
@@ -1,53 +1,67 @@
 const symbolRace = require('./symbol-race');
 
+/**
+ * Check if the an object is Promise
+ */
+function isPromise(obj) {
+  return obj && obj instanceof Promise;
+}
+
+/**
+ * co function to be exported
+ */
 function co(gen) {
-	return new Promise((resolve, reject) => {
-		const pointer = gen();
-		function exec(err, v) {
-			try {
-				const pResult = err ? pointer.throw(err) : pointer.next(v);
-				// Is generator done?
-				if (pResult.done) {
-					// Generator is done
-					// Resolve the Promise returned by co
-					resolve(pResult.value);
-				} else {
-					// Generator is not done yet
-					function onResolved(result) {
-						exec(null, result);
-					}
-					function onRejected(err) {
-						exec(err);
-					}
-					const pResultValue = pResult.value;
-					// Is yield a Promise?
-					if (pResultValue instanceof Promise) {
-						// Yield is a Promise, deal with it
-						pResultValue.then(onResolved, onRejected);
-					} else
-					// Is Yield an array of Promises?
-					if (pResultValue instanceof Array &&
-							pResultValue.some((item) => (item instanceof Promise))) {
-						// Yield is an array of Promises
-						// Which way should we wrap these Promises? (all/race)
-						const wrap = pResultValue[0] === symbolRace ?
-							Promise.race.bind(Promise) :
-							Promise.all.bind(Promise);
-						wrap(pResultValue.filter((item) => (item instanceof Promise)))
-							.then(onResolved, onRejected);
-					} else {
-						// Yield is not a Promise, nor an array of Promises, pass the yield itself
-						onResolved(pResultValue);
-					}
-				}
-			} catch(e) {
-				// Uncaught errors in generator
-				// Reject the Promise returned by co
-				reject(e);
-			}
-		}
-		exec();
-	});
+  return new Promise((resolve, reject) => {
+    const pointer = gen();
+    function exec(err, v) {
+      const onResolved = (result) => {
+        exec(null, result);
+      },
+      onRejected = (err) => {
+        exec(err);
+      };
+      try {
+        // If error occurs, throw it, else continue execution
+        const pResult = err ? pointer.throw(err) : pointer.next(v);
+        // Is generator done?
+        if (pResult.done) {
+          // Generator is done
+          // Resolve the Promise returned by co
+          resolve(pResult.value);
+        } else {
+          // Generator is not done yet, deal with yield
+          const pResultValue = pResult.value;
+          // Is yield a Promise?
+          if (isPromise(pResultValue)) {
+            // Yield is a Promise
+            pResultValue.then(onResolved, onRejected);
+          } else
+          // Is Yield an array of Promises, which contains at least one Promise?
+          if (pResultValue && pResultValue instanceof Array &&
+              pResultValue.some(isPromise)) {
+            // Yield is an array of Promises
+            // Which way should we treat these Promises? (all/race)
+            if (pResultValue[0] === symbolRace) {
+              Promise.race(pResultValue.filter(isPromise))
+                .then(onResolved, onRejected);
+            } else {
+              Promise.all(pResultValue.filter(isPromise))
+                .then(onResolved, onRejected);
+            }
+          } else {
+            // Yield is not a Promise, nor an array of Promises, pass the yield itself
+            onResolved(pResultValue);
+          }
+        }
+      } catch(e) {
+        // Uncaught errors in generator
+        // Reject the Promise returned by co
+        reject(e);
+      }
+    }
+    // Execute generator function initially with no error and no previous value
+    exec(null, null);
+  });
 }
 
 module.exports = co;
